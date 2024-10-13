b136655f84fc8938ded3adc2b2fd0cd3b9c66322
Jihanchy <habibullahalquaderi2005@gmail.com>
2024-10-14 01:38:01 +0600

    added aome common functionality

--- function.js
+++ function.js
@@ -10,9 +10,11 @@ const loadingAllPhone = async(status,brandName) => {
 }
 
 const displayAllPhone = (phones) => {
+    
+    document.getElementById('phone-container').innerHTML=""
     const phoneContainer = document.getElementById('phone-container')
     phones.forEach(phone => {
-        const {brand,phone_name,slug,image} = phone;
+        const {phone_name,slug,image} = phone;
         const card = document.createElement('div')
         card.classList="card shadow-md p-5"
         card.innerHTML=`
@@ -26,7 +28,7 @@ const displayAllPhone = (phones) => {
           <h2 class="card-title text-2xl font-bold">${phone_name}</h2>
           <p>${slug}</p>
           <div class="card-actions">
-            <button class="btn btn-md text-white bg-btn_primary">Show Details</button>
+            <button onclick="showDetails('${slug}')" class="btn btn-md text-white bg-btn_primary">Show Details</button>
           </div>
         </div>
         `
@@ -35,8 +37,38 @@ const displayAllPhone = (phones) => {
 }
 
 // details
-const showDetails = async(id) => {
-    
+const showDetails = async(slug) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+    const data =await response.json()
+    const {name,image,mainFeatures,releaseDate,brand,others} = data.data
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.innerHTML=  `
+    <dialog id="my_modal_5" class="modal modal-bottom md:modal-center  sm:modal-middle">
+          <div class="modal-box">
+            <div class="py-4 flex items-center justify-center bg-slate-100 rounded-md">
+            <img src="${image}"/>
+            </div>
+            <h3 class="text-2xl font-bold">${name}</h3>
+            <p class="py-4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+            <p>Storage: ${mainFeatures.storage}</p>
+            <p>Display size: ${mainFeatures.displaySize}</p>
+            <p>Chipset: ${mainFeatures.chipSet}</p>
+            <p>Memory: ${mainFeatures.memory}</p>
+            <p>Slug: ${slug}</p>
+            <p>Release Date: ${releaseDate}</p>
+            <p>Brand: ${brand}</p>
+            <p>GPS: ${others?.GPS}</p>
+            <div class="modal-action">
+              <form method="dialog">
+                <!-- if there is a button in form, it will close the modal -->
+                <button class="btn btn-error text-white">Close</button>
+              </form>
+            </div>
+          </div>
+    </dialog>
+    `
+    my_modal_5.showModal()
+
 }
 const showAllBtn = () => {
     loadingAllPhone(true)
